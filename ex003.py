99c8333053162c30b8fb690af541d72147c89205
daophin <aluis.aneves17@gmail.com>
2023-10-16 14:35:58 -0300

    att. ex003

---
+++ ex003.py
@@ -0,0 +1,19 @@
+from time import sleep
+# Curos em Vídeo de Python
+# Exercício 3 - somando dois números
+
+print('-=-=-=-=-=-')
+print('Bem Vindo!')
+print('-=-=-=-=-=-')
+sleep(1)
+print('...')
+sleep(2)
+print('Calculadora de somar dois números!')
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+s = n1 + n2
+sleep(1)
+print('Calculando ...')
+sleep(1.5)
+print(f'A soma dos valores {n1} + {n2} é igual a {s}')
+print('FIM DA OPERAÇÃO!')
