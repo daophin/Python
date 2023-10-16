3fc539b38f49f895bc02217af29dfc1fa8480fb8
daophin <aluis.aneves17@gmail.com>
2023-10-16 16:47:15 -0300

    Create aula 6

---
+++ Aulas/Aula 6.py
@@ -0,0 +1,11 @@
+# Curso em Vídeo de Python - Aula 6
+# Nesta aula iremos aprender os Tipos Primitivos
+# Temos 4 principais tipos primitivos, int(), floot(), bool() e str()
+# Na sequencia fica assim: inteiro, ponto flutuante, booleano e string
+# Podemos aplicar esses tipos diretamente na variável que quisermos inserir
+# Descomente o bloco do código a abaixo e rode para ver o que acontece.
+"""n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite um outro valor: '))
+s = n1 + n2
+print(f'A soma é {s}')"""
+# Para comentar novamente insira 3 aspas duplas"""no início e no final do código
